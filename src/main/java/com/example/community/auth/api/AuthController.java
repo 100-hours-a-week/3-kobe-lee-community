11f72628f10fafae6d2c64280f3e536fc574093f
yseo14 <lmslys14@naver.com>
2025-10-07 00:21:23 +0900

    feat: 로그아웃 API 구현

--- src/main/java/com/example/community/auth/api/AuthController.java
+++ src/main/java/com/example/community/auth/api/AuthController.java
@@ -2,10 +2,13 @@ package com.example.community.auth.api;
 
 import com.example.community.auth.api.dto.LoginRequest;
 import com.example.community.auth.api.dto.LoginResponse;
+import com.example.community.auth.api.dto.LogoutResponse;
 import com.example.community.auth.application.service.AuthService;
 import com.example.community.global.response.ApiResponse;
 import com.example.community.global.response.code.status.SuccessStatus;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,11 @@ public class AuthController {
     public ApiResponse<LoginResponse> login(@RequestBody LoginRequest request) {
         LoginResponse loginResponse = authService.login(request);
         return ApiResponse.onSuccess(SuccessStatus.LOGIN_SUCCESS, loginResponse);
+    }
+
+    @DeleteMapping
+    public ApiResponse<LogoutResponse> logout(HttpServletRequest request) {
+        LogoutResponse response = authService.logout(request);
+        return ApiResponse.onSuccess(SuccessStatus.LOGOUT_SUCCESS, response);
     }
 }
